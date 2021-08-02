fb7167eda70569f87bd75e97ec9daa9e9ae6e120
may <soo.toance@gmail.com>
2021-08-03 01:49:04 +0900

    롬북 설정 및 dto 추가 + 테스트코드

--- src/test/java/com/toance/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/toance/book/springboot/web/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.toance.book.springboot;
+package com.toance.book.springboot.web;
 
 import com.toance.book.springboot.web.HelloController;
 import org.junit.Test;
@@ -8,9 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -25,5 +26,20 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk()) // mvc.perform의 결과 검증
                 .andExpect(content().string(hello)); // mvc.perform의 결과 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                    get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount)) // param의 경우 값으로 string만 허용
+                )
+                .andExpect(status().isOk()) // mvc.perform의 결과 검증
+                .andExpect(jsonPath("$.name", is(name))) // mvc.perform의 결과 검증
+                .andExpect(jsonPath("$.amount", is(amount))); // mvc.perform의 결과 검증
     }
 }
